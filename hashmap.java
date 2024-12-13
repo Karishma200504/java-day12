7891b5eae3a29a31e467e7c938d4ec6b4ac50a33
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-13 16:00:27 +0530

    Add files via upload

---
+++ hashmap.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class hashmap{
+    public static void main(String args[]){
+        HashMap<String,String> a=new HashMap<>();
+        a.put("sNo","01");
+        a.put("sName","Muraja");
+        System.out.println(a);
+    }
+}
