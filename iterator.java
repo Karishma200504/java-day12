859e7dcd4b7d315c2552a3416f3a6f34e555f30f
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-13 14:32:03 +0530

    Add files via upload

---
+++ iterator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class iterator{
+    public static void main(String args[]){ 
+    LinkedHashSet<String> a=new LinkedHashSet<>();
+    a.add("hello");
+    a.add("hello");
+    a.add("students");
+    a.add("rajesh");
+    a.add("students");
+    //printing using iterator
+    Iterator itr=a.iterator();
+    while(itr.hasNext()){
+        System.out.print(itr.next()+" ");
+    }
+}
+}
